0ec40862611ea11cf522d63a2b2c36abb10e24c1
Xhulio Hasani <xhulioh@gmail.com>
2019-09-29 06:13:16 -0400

    repairing all the things Loic broke...

--- electron/src/camera.js
+++ electron/src/camera.js
@@ -1,18 +1,17 @@
-let { coordsToDirection, Points, Direction, HandsPosition } = require("./src/coordsToDirection")
-let io = require("socket.io-client")
+let { coordsToDirection, Points, Direction, HandsPosition } = require("./src/coordsToDirection");
+let io = require("socket.io-client");
 
 const socket = io('http://localhost:3000');
 
 socket.on('connect', function() {
-  console.log("Socket Connected")
-})
+  console.log("Socket Connected");
+});
 
 const loader = document.getElementById("main-loader");
 const video = document.getElementById("myvideo");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
-let updateNote = document.getElementById("updatenote");
 
 let isVideo = false;
 let model = null;
@@ -28,24 +27,18 @@ function startVideo() {
     handTrack.startVideo(video).then(function (status) {
         console.log("video started", status);
         if (status) {
-            updateNote.innerText = "Video started. Now tracking"
-            isVideo = true
-            runDetection()
-        } else {
-            updateNote.innerText = "Please enable video"
+            isVideo = true;
+            runDetection();
         }
     });
 }
 
 function toggleVideo() {
     if (!isVideo) {
-        updateNote.innerText = "Starting video"
         startVideo();
     } else {
-        updateNote.innerText = "Stopping video"
         handTrack.stopVideo(video)
         isVideo = false;
-        updateNote.innerText = "Video stopped"
     }
 }
 
@@ -53,24 +46,22 @@ function toggleVideo() {
 
 function runDetection() {
     model.detect(video).then(predictions => {
-
-
         if (predictions.length == 2) {
             let leftHand;
             let rightHand;
             if (predictions[0].bbox[0] < predictions[1].bbox[0]) {
-                leftHand = new Points(predictions[0].bbox[0], predictions[0].bbox[1])
-                rightHand = new Points(predictions[1].bbox[0], predictions[1].bbox[1])
+                leftHand = new Points(predictions[0].bbox[0], predictions[0].bbox[1]);
+                rightHand = new Points(predictions[1].bbox[0], predictions[1].bbox[1]);
             } else {
-                leftHand = new Points(predictions[1].bbox[0], predictions[1].bbox[1])
-                rightHand = new Points(predictions[0].bbox[0], predictions[0].bbox[1])
+                leftHand = new Points(predictions[1].bbox[0], predictions[1].bbox[1]);
+                rightHand = new Points(predictions[0].bbox[0], predictions[0].bbox[1]);
             }
-            let handPosition = new HandsPosition(leftHand, rightHand)
-            const output = coordsToDirection(handPosition)
+            let handPosition = new HandsPosition(leftHand, rightHand);
+            const output = coordsToDirection(handPosition);
 
-            console.log("OUTPUT", output)
+            console.log("OUTPUT", output);
 
-            if (output) socket.emit('transfer', output)
+            if (output) socket.emit('transfer', output);
         }
     
 
@@ -84,10 +75,10 @@ function runDetection() {
 // Load the model.
 handTrack.load(modelParams).then(lmodel => {
     // detect objects in the image.
-    model = lmodel
-    loader.style.display = "none"
-    console.info("Model loaded!")
-    trackButton.disabled = false
+    model = lmodel;
+    loader.style.display = "none";
+    console.info("Model loaded!");
+    trackButton.disabled = false;
 });
 
 
